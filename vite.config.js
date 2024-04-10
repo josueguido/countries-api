9236c41bf0b67169eef196e98131f90618282965
Your Name <you@example.com>
2024-04-09 20:46:46 -0600

    Instalacion de MillionJs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   test: {
     globals: true,
     environment: 'jsdom',
